a14334b660cc82f8db56dcd6528aa12480b26593
Brissett <brissettor@jncb.com>
2018-10-19 11:05:22 -0500

    update out method

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 
-
 app.use(express.json());
 
 const genres = [
@@ -20,20 +19,20 @@ function validateGenres(genres){
 
 app.get('/', (req, res) =>{
     res.send('Welcome to the genre service');
-})
+});
 
 //Get all genres
 app.get('/api/genres', (req, res) => {
     res.send(genres);
-})
+});
 
 //Get genre per ID
 app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.body.id));
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
     if(!req.body.id) return res.status(404).send('The genre with the given id was not found');
 
     res.send(genre);
-})
+});
 
 app.post('/api/genres', (req,res) => {
     //Object destructing to retrieve exact value from object thats need
@@ -52,30 +51,42 @@ app.post('/api/genres', (req,res) => {
     genres.push(genre);
     res.send(genre);
 
-})
+});
 
-app.put('/api/genres', (res, req) => {
-    //find course for the id specified and parse the id as an integer
-    const genre = genres.find(genre => genre.id = parseInt(req.body.id));
+app.put('/api/genres/:id', (req, res) => {
+    //Look up the course
+    //If not existing, retun 404
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found');
+
+    //Validate
+    //If invalid, return 400 - Bad request
+    const {error} = validateGenres(req.body);
+    if (error){
+        return res.status(400).send(error.details[0].message);
+    }
+    
+    //Update course
+    genre.name = req.body.name;
+    //return the updated course
+    res.send(genre);
+
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(genre => genre.id === parseInt(req.params.id));
     //Return not found if the id is invalid
     if(!genre) return res.status(404).send('The genre with the given id was not found');
 
-    //Validate the request
-    const {error} = validateGenres(req.body)
-    if(error){
-        return res.status(400).send(error.details[0].message)
-    }
+    //Delete
+    //Find index of course in array 
+    const index = genres.indexOf(genre);
+    genres.splice(index,1);
 
-    //update existing object found
-    genre.name = req.body.name;
-    //return updated object
+    //return object that was deleted
     res.send(genre);
 
-})
-
-app.delete('/api/genre', (req, res) => {
-    
-})
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port,() => console.log(`Listening on port ${port}`));

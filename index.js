d2eba193fca11d455ba00530799989f252342863
Brissett <brissettor@jncb.com>
2018-10-19 10:28:27 -0500

    adding update http method

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 
-app.use(expess.json());
+app.use(express.json());
 
 const genres = [
     {id: 1, name: 'Documentary'},
@@ -48,9 +48,33 @@ app.post('/api/genres', (req,res) => {
         id: genres.length + 1,
         name: req.body.name
     };
-    
-    genres.push(genre);
 
+    genres.push(genre);
+    res.send(genre);
+
+})
+
+app.put('/api/genres', (res, req) => {
+    //find course for the id specified and parse the id as an integer
+    const genre = genres.find(genre => genre.id = parseInt(req.body.id));
+    //Return not found if the id is invalid
+    if(!genre) return res.status(404).send('The genre with the given id was not found');
+
+    //Validate the request
+    const {error} = validateGenres(req.body)
+    if(error){
+        return res.status(400).send(error.details[0].message)
+    }
+
+    //update existing object found
+    genre.name = req.body.name;
+    //return updated object
+    res.send(genre);
+
+})
+
+app.delete('/api/genre', (req, res) => {
+    
 })
 
 const port = process.env.PORT || 3000;

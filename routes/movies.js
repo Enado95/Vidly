5c7ff65616489d0f118209489294ff64a05914c4
enado95 <brissettodane@gmail.com>
2018-11-13 11:10:15 -0500

    ...

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
-const { Movie, validate } = require('../models/movies');
-const { Genre } = require('../models/genres')
+const { Movie, validate } = require('../models/movie');
+const { Genre } = require('../models/genre')
 const mongoose = require('mongoose');
 const express = require('express');
 

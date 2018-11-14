85d70a4bc9fa2e79ede8a725704d9c605b079db4
enado95 <brissettodane@gmail.com>
2018-11-14 09:45:34 -0500

    adding authentication

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 Joi.objectId =  require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const express = require('express');
+const auth = require('./routes/auth');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
@@ -18,14 +19,11 @@ mongoose
 
 app.use(express.json());
 app.use('/api/genres', genres);
-
 app.use('/api/customers', customers);
-
 app.use('/api/movies', movies);
-
 app.use('/api/rentals', rentals);
-
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));

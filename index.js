1c899fed4b78b0615a24cd6ae1d82fd66e21b9ec
ODane <ODane@ODanes-MacBook-Pro.local>
2018-11-16 22:50:21 -0500

    .

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const logger = require('./middleware/logger');
-const winston = require('winston');
 const express = require('express');
 const app = express();
 
@@ -8,7 +7,6 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => logger.infoLog.log({level: 'info', message: `Listening on port ${port}`}));

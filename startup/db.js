13c27b397a48f53fdaaf3759cc816f3bd6ce3f1b
ODane <ODane@ODanes-MacBook-Pro.local>
2018-11-19 22:51:05 -0500

    ..

--- startup/db.js
+++ startup/db.js
@@ -1,20 +1,18 @@
 const mongoose = require("mongoose");
 const logger = require("../middleware/logger");
+const config = require("config");
 
 module.exports = function() {
   mongoose
     .connect(
-      "mongodb+srv://cluster0-6aldy.mongodb.net/vidly?retryWrites=true",
+      config.get("Movies.dbConfig.host"),
       {
-        authSource: "admn",
-        auth: {
-          user: "superuser",
-          password: "greatpassword"
-        },
         useNewUrlParser: true,
         useFindAndModify: false,
-        useCreateIndex: true
-      }
+        useCreateIndex: true,
+        uri_decode_auth: true 
+      },
+      function(err, db) { }
     )
     .then(() =>
       logger.infoLog.log({ level: "info", message: "Connected to MongoDB.." })

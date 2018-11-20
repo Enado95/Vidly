131ce5edc42dddcdc7df4bc1a124a2db1799b600
ODane <ODane@ODanes-MacBook-Pro.local>
2018-11-19 20:34:56 -0500

    log update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const logger = require('./middleware/logger');
 const express = require('express');
 const app = express();
 
-require('./startup/logging');
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/config')();
 require('./startup/validation')();
